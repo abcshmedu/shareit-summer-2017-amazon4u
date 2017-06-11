1248a7c26192ecc6584cefac6cb3a7882a6f9f9b
whaph <whaph@outlook.com>
2017-06-11 16:50:46 +0200

    ...

--- src/main/java/edu/hm/shareit/application/ShareitApplication.java
+++ src/main/java/edu/hm/shareit/application/ShareitApplication.java
@@ -4,7 +4,7 @@ import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
-import edu.hm.shareit.observer.ShareitServletContextListener;
+import edu.hm.shareit.listener.ShareitServletContextListener;
 
 import javax.inject.Inject;
 
