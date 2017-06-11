4b9e9c50de4988ae7d6cbb2c0e93df60402b46fa
whaph <whaph@outlook.com>
2017-06-11 17:10:21 +0200

    ...

--- src/main/java/edu/hm/shareit/application/ShareitApplication.java
+++ src/main/java/edu/hm/shareit/application/ShareitApplication.java
@@ -1,10 +1,10 @@
 package edu.hm.shareit.application;
 
+import edu.hm.shareit.listener.ShareitServletContextListener;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.jvnet.hk2.guice.bridge.api.GuiceBridge;
 import org.jvnet.hk2.guice.bridge.api.GuiceIntoHK2Bridge;
-import edu.hm.shareit.listener.ShareitServletContextListener;
 
 import javax.inject.Inject;
 
@@ -12,6 +12,7 @@ import javax.inject.Inject;
  * Application class to enable guice within jersey.
  */
 class ShareItApplication extends ResourceConfig {
+
     @Inject
     public ShareItApplication(ServiceLocator serviceLocator) {
         GuiceBridge.getGuiceBridge().initializeGuiceBridge(serviceLocator);
